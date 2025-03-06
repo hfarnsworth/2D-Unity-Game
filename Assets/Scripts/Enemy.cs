39e020cca8ecdb9f53dd601995b3884e0f650b30
Flak <havilahfarnsworth@gmail.com>
2025-03-06 16:22:47 -0700

    final changes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -60,6 +60,7 @@ public class Enemy : MonoBehaviour
             _speed = 0;
             _audioSource.Play();
 
+            Destroy(GetComponent<Collider2D>());
             Destroy(this.gameObject, 2.5f);
         }
         
@@ -75,6 +76,7 @@ public class Enemy : MonoBehaviour
             _speed = 0;
             _audioSource.Play();
 
+            Destroy(GetComponent<Collider2D>());
             Destroy(this.gameObject, 2.5f);
         }
     }
